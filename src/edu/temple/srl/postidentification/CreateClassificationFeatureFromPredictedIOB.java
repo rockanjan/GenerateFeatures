61a62f9e0a4caf12273aba2d1a3831a0196e2549
Anjan Nepal <rockanjan@gmail.com>
2012-07-16 19:37:38 -0400

    added java file to create feature from predicted BIO

---
+++ src/edu/temple/srl/postidentification/CreateClassificationFeatureFromPredictedIOB.java
@@ -0,0 +1,56 @@
+package edu.temple.srl.postidentification;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import sun.applet.Main;
+
+public class CreateClassificationFeatureFromPredictedIOB {
+	public static void main(String[] args) throws IOException {
+		String outputFeatureFile = "/home/anjan/work/srl/jul11/dev.predictediob.classification"; 
+		String fileWithOriginalIOB = "/home/anjan/work/conll05-backup/combined/dev.combined.final";
+		String fileWithPredictedIOB = "/home/anjan/work/srl/jul11/dev.iden.tag";
+		int iobField = 7; //human index
+		//replace the IOB field
+		BufferedReader br1 = new BufferedReader(new FileReader(fileWithOriginalIOB));
+		BufferedReader br2 = new BufferedReader(new FileReader(fileWithPredictedIOB));
+		
+		String finalFileWithPredictedIOB = "/tmp/predicted.final"; 
+		PrintWriter pw = new PrintWriter(finalFileWithPredictedIOB);
+		
+		String line1 = "";
+		String line2 = "";
+		
+		while( (line1 = br1.readLine()) != null && (line2 = br2.readLine()) != null) {
+			String[] splittedOriginal = line1.split("(\\s+|\\t+)");
+			String[] predictedSplitted = line2.split("(\\s+|\\t+)");
+			if(! line1.trim().equals("")) {
+				for(int i=0; i<iobField-1; i++){
+					pw.print(splittedOriginal[i] + "\t");
+				}
+				
+				pw.print(predictedSplitted[predictedSplitted.length - 1] + "\t");
+				for(int i=iobField; i<splittedOriginal.length; i++){
+					pw.print(splittedOriginal[i] + "\t");
+				}				
+			} else {
+				if( ! line2.trim().equals("")) {
+					System.err.println("Original and predicted files are not aligned");
+				}
+			}
+			pw.println();
+			pw.flush();
+		}
+		br1.close();
+		br2.close();
+		pw.close();
+		
+		//now create the new features using predicted IOB
+		String[] arguments = {finalFileWithPredictedIOB, outputFeatureFile};
+		System.out.println("Creating features...");
+		edu.temple.srl.base.Main.main(arguments);
+		System.out.println("Created feature file with the predicted IOB at " + outputFeatureFile);
+	}
+}
